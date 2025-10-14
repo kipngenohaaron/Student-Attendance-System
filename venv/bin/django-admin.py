fb030dd6a059acef449b2343161551f0c9a79cf8
kipngenohaaron <kipngenohaaron@gmail.com>
2025-10-14 20:00:24 +0300

    updating the files

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/kipngenoh/Desktop/CODING SOLUTIONS/Student-Attendance-System/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
